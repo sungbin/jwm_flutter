4674f372ef8d34af054be901c70d5d1da0a1dd5c
sungbin <21700613@handong.edu>
2021-03-26 11:02:29 +0900

    image loading

--- lib/mexample.dart
+++ lib/mexample.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -17,32 +18,43 @@ class _MexampleState extends State<Mexample> {
   void initState() {
     super.initState();
     _fetch(car_info_url);
+    Timer(Duration(seconds: 5), () => print('done'));
   }
 
   String _html;
   int _len = 0;
+  Information info = null;
   void _fetch(String link) async {
     final http.Response response = await http.get(link);
     log(response.statusCode.toString());
     setState(() {
       _html = utf8.decode(response.bodyBytes);
+      info ??= Information(_html);
     });
   }
 
+// TODO: spanish screenshot
   @override
   Widget build(BuildContext context) {
-    Information info = Information(_html);
-    log(info.user['jwm1992'].selled_car_list.toString());
-    Text html_t = Text(
-      _html,
-      // info.user['jwm1992'].selled_car_list.toString(),
-      style: TextStyle(
-        fontSize: 12,
-      ),
+    // log(info.user['jwm1992'].selled_car_list.toString());
+    return Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        Container(
+          child: Image.asset(
+            'asset/jwm_loading.png',
+            fit: BoxFit.fitHeight,
+          ),
+        ),
+        Align(
+          alignment: FractionalOffset(0.5, 0.82),
+          child: Container(
+            child: CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            ),
+          ),
+        ),
+      ],
     );
-    return Expanded(
-        flex: 1,
-        child: SingleChildScrollView(
-            child: html_t, scrollDirection: Axis.vertical));
   }
 }
